645e52a0de9e2e6df5cc6690900e9b268236997a
Heart Beat Studio <blacklight728@gmail.com>
2015-10-03 22:04:09 -0400

    Add associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
+  has_one :garden
+  has_many :products, through: :garden
+  has_many :messages
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes["password"] }
   validates :password, confirmation: true, if: -> { new_record? || changes["password"] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes["password"] }
