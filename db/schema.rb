b149d6a46f278d5ab262ce3afff0fc8fed65ef1f
Irwin <irwinchan@me.com>
2015-10-04 15:30:43 -0400

    Add description column to product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151003224633) do
+ActiveRecord::Schema.define(version: 20151004192833) do
 
   create_table "gardens", force: :cascade do |t|
     t.integer  "user_id"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20151003224633) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "name"
     t.integer  "garden_id"
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
