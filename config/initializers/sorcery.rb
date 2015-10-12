b8ba8fb0014bcbf917ca99a5952ed47c182ec0df
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-2.local>
2015-10-11 21:28:38 -0400

    Add Facebook login with sorcery

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -76,7 +76,7 @@ Rails.application.config.sorcery.configure do |config|
   # What providers are supported by this app, i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid, :salesforce] .
   # Default: `[]`
   #
-  config.external_providers = [:github, :twitter]
+  config.external_providers = [:github, :twitter, :facebook]
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -116,13 +116,13 @@ Rails.application.config.sorcery.configure do |config|
   config.twitter.user_info_mapping = {:email => "screen_name"}
 
   #
-  # config.facebook.key = ""
-  # config.facebook.secret = ""
-  # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
-  # config.facebook.user_info_mapping = {:email => "name"}
-  # config.facebook.access_permissions = ["email", "publish_actions"]
-  # config.facebook.display = "page"
-  # config.facebook.api_version = "v2.2"
+  config.facebook.key = "897009703681282"
+  config.facebook.secret = "2d249df507ca31f3800ed012a22a0d97"
+  config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
+  config.facebook.user_info_mapping = {:email => "name"}
+  config.facebook.access_permissions = ["email", "publish_actions"]
+  config.facebook.display = "page"
+  config.facebook.api_version = "v2.2"
   #
   config.github.key = Figaro.env.github_key
   config.github.secret = Figaro.env.github_secret
