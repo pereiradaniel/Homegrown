54f50f897883a3175d152b8bad7f384b7a3b5c98
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-2.local>
2015-10-11 20:04:27 -0400

    Add Twitter Login
    Error with twitter login in sorcery bug will
    be fixed in sorcery 1.0

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -112,7 +112,7 @@ Rails.application.config.sorcery.configure do |config|
 
   config.twitter.key = Figaro.env.twitter_key
   config.twitter.secret = Figaro.env.twitter_secret
-  config.twitter.callback_url = "http://127.0.0.1:3000/oauth/callback?provider=twitter"
+  config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   config.twitter.user_info_mapping = {:email => "screen_name"}
 
   #
