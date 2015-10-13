1f0e45c895efb7f03129550bb36cbb036088e52e
Daniel Pereira <pereiradpg@yahoo.ca>
2015-10-13 16:53:45 -0400

    Updated seed file to add image to products and more random users.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,5 +2,6 @@ class Conversation < ActiveRecord::Base
 	has_many :messages
 	belongs_to :product
 	belongs_to :sender, class_name: User
+	belongs_to :receiver, class_name: User
 	has_one :trade
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,38 @@
 #   )
 # end
 
+# ary[rand(ary.size)]
+
+picture_array = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+postal_code_array = ["M5H 1W7", "M7A 1Y7", "M1J 2E1", "L4B 3P4", "M9C 2Y2", "M5H 2M3", "M2N 5V7", "M2J 5A7"]
+# postal_code: postal_code_array[rand(ary.size)]
+product_name_array = ["Apple", "Pepper", "Potato", "Garlic", "Sweet potato", "Hot pepper", "Sweet pepper",
+  "Chives", "Lettuce", "Tomato", "Roma tomato", "Green apple", "Macintosh apple", "Uncle Bob's Super Gala Apple",
+  "Zucchini", "Uncle Orazio's Special Greenhouse Zucchinis", "Hydroponic Tomatoes", "Basil", "Oregano", "Dried and cured Oregano",
+  "Spanish Onion", "Onion", "Green Onion"]
+email_array = ["hotmail", "yahoo", "gmail"]
+product_description_array = ["Juicy and tender", "Picked last week!", "My first attempt growing these.", "My special growing technique!",
+  "Made with love!", "For those of the organic persuasion.", "Grown in dirt.", "All-natural ferilizer", "Straight from my backyard.",
+  "Special genetically modified seeds for Canadian climate.", "From the heritage seed bank.  An old-school produce.", "Grown with seeds from Africa.",
+  "My grandmother's special seeds, passed down through generations."]
+trade_array = ["For free!  1 x bushel per request.  Send a request, and I'll tell you where to pick it up.", "I would like to trade with whatever you are growing.",
+  "I am selling these for $2 / bunch.", "I am giving these away!", "Would like to hear what you have to trade, please send a request for negotiation.",
+  "I want to trade these for some nice herbs and spices, nothing in particular.  Send me a request and we can discuss.", "$5 / bushel OBO.",
+  "Would love to trade with someone who is good at growing corn.", "Not sure what I'd like to trade these for, or you can purchase for $10 for a large bushel, $5 for small bushel."]
+user_names = ["Farmer Joe", "Miguel", "Johnny", "Sally", "Uncle Bob", "Jessie", "Big Mike", "Orlando", "Diana", "Ravinder", "Rohini",
+  "Lisa", "James", "Hong", "Simon", "Imran", "Masterful Mina", "Intelligent Ilia", "Mega Matt", "Naturalie Natalie", "Erik 'The Grilled Cheeser' Dohnberg", "Adrian aka 'Jaclyn 2.0'"]
+request_array = ["I want to buy right away for the price you are asking!  Can you supply 5 or 6?", "Could you trade me 3 of these for 5 of my special basil leaves?  They are already prepared for use",
+  "Wow I would love to have 10 of these if possible, but could you give me a 10% discount on the bulk order?", "I don't have anything to trade, but could you sell me some?",
+  "That is a great price!  Can I pick up 3x by this Friday?", "Hey those look pretty good!  Did you see my special apples?  Mayby we could trade a bushel for a bushel??? :)",
+  "What species did you grow?  I may be interested in paying top $$$ for a nice big jar.", "I like these :) Can you sell me 3 bushels and can I pick up by Wednesday?",
+  "Wow all your stuff looks great!  How fresh are they?  I want a bag of those before the weekend, I'm making a pie!", "Those would look good in my grandma's recipe, can I buy a jar and pick up tomorrow?",
+  "Awesome produce!  I have lots to trade, check out my page and see waht you like!  :P", "Dude, your produce looks great!  I have nothing to trade, but can I buy some of these???"]
+message_array = ["Ok!", "Not sure, let me check.", "I only have 2 left!", "That sounds good.", "Can you pick up by tomorrow?", "I will be away until Tuesday.",
+  "Sorry I'm all out!  I have to de-list the product.", "There's more than enough, come get it!", "These will be ready next week."]
+email_handle = ["super_", "mega_", "greasy_", "the_amazing_", "litte_", "big_", "rasta_", "bigmouth_", "blue_eyes_", "old_", "young_", "dude_"]
+email_nicks = ["dave", "strongman", "viking", "larry", "holmes", "king", "princess", "goofy", "granny", "gramps", "daddy", "skater", "biker",
+  "redhead", "rebel", "master_chef", "dancer", "pianist", "noodles", "baldy", "trailer_park_guy"]
+domain_array = [".com", ".ca", ".cx", ".biz", ".uk", ".org", ".ru", ".net", ".cn", ".jp", ".pt", ".br", ".ge", ".ir", ".au", ".nz", ".ro", ".pl"]
 
 5.times do |i|
   x = i + 1
@@ -33,7 +65,8 @@
     name: "Product #{x}a",
     description: "Description #{x}a",
     trade_info: "Trade info #{x}a",
-    postal_code: "M5H 1W7"    
+    postal_code: "M5H 1W7",
+    image: open(picture_array[rand(picture_array.size)] + ".jpg")
     })
 
   Product.create!({
@@ -41,7 +74,8 @@
     name: "Product #{x}b",
     description: "Description #{x}b",
     trade_info: "Trade info #{x}b",
-    postal_code: "M5H 1W7"
+    postal_code: "M5H 1W7",
+    image: open(picture_array[rand(picture_array.size)] + ".jpg")
     })
 
 end
@@ -57,7 +91,7 @@ end
 
   Garden.create!({
     user_id: x,
-    postal_code: "M1S 1B7"
+    postal_code: "M9C 2Y2"
     })
 
   Product.create!({
@@ -65,7 +99,7 @@ end
     name: "Product #{x}a",
     description: "Description #{x}a",
     trade_info: "Trade info #{x}a",
-    postal_code: "M1S 1B7"
+    postal_code: "M9C 2Y2"
     })
 
   Product.create!({
@@ -73,7 +107,7 @@ end
     name: "Product #{x}b",
     description: "Description #{x}b",
     trade_info: "Trade info #{x}b",
-    postal_code: "M1S 1B7"
+    postal_code: "M9C 2Y2"
     })
 
 end
@@ -190,3 +224,74 @@ Trade.create!({
   conversation_id: 4,
   success: true
 })
+
+50.times do |i|
+    User.create!({
+      name: user_names[rand(user_names.size)],
+      email: email_handle[rand(email_handle.size)] + email_nicks[rand(email_nicks.size)] + "@" + email_array[rand(email_array.size)] + domain_array[rand(domain_array.size)],
+      password: "1111",
+      password_confirmation: "1111"    
+      })
+
+    Garden.create!({
+      user_id: User.last.id,
+      postal_code: postal_code_array[rand(postal_code_array.size)]
+      })
+
+    Product.create!({
+      garden_id: Garden.last.id,
+      name: product_name_array[rand(product_name_array.size)],
+      description: product_description_array[rand(product_description_array.size)],
+      trade_info: trade_array[rand(trade_array.size)],
+      postal_code: Garden.last.postal_code,
+      image: open(picture_array[rand(picture_array.size)] + ".jpg")
+      })
+
+    Product.create!({
+      garden_id: Garden.last.id,
+      name: product_name_array[rand(product_name_array.size)],
+      description: product_description_array[rand(product_description_array.size)],
+      trade_info: trade_array[rand(trade_array.size)],
+      postal_code: Garden.last.postal_code,
+      image: open(picture_array[rand(picture_array.size)] + ".jpg")
+      })
+
+    Product.create!({
+      garden_id: Garden.last.id,
+      name: product_name_array[rand(product_name_array.size)],
+      description: product_description_array[rand(product_description_array.size)],
+      trade_info: trade_array[rand(trade_array.size)],
+      postal_code: Garden.last.postal_code,
+      image: open(picture_array[rand(picture_array.size)] + ".jpg")
+      })
+
+  # rand_product = Product.order("RANDOM()").first
+
+ offset = rand(Product.count)
+ rand_product = Product.offset(offset).first
+
+  Conversation.create!({
+    request: request_array[rand(request_array.size)],
+    product_id: rand_product.id,
+    sender_id: User.last.id,
+    receiver_id: rand_product.garden.user.id
+    })
+
+  5.times do |i|
+      x = i + 1
+      Message.create!({
+        content: message_array[rand(message_array.size)],
+        conversation_id: Conversation.last,
+        sender_id: Conversation.last.sender.id,
+        receiver_id: Conversation.last.receiver.id
+        })
+
+      Message.create!({
+        content: message_array[rand(message_array.size)],
+        conversation_id: Conversation.last,
+        sender_id: Conversation.last.receiver.id,
+        receiver_id: Conversation.last.sender.id
+        })
+    end
+
+end
