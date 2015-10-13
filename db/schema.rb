f925973bf91e696d528f958d1009c6131f63eb86
Ruby Li <rubyli@Ruby-Lis-MacBook-Pro-2.local>
2015-10-13 10:39:31 -0400

    Styling the Product index page
    Front-end styling for the top menu
    bar and basic page layout.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011182041) do
+ActiveRecord::Schema.define(version: 20151011225027) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id",    null: false
