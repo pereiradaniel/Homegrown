2f854a7d6b09c3db2ce84bfacd4d5f18c7a37920
Heart Beat Studio <blacklight728@gmail.com>
2015-10-18 22:13:30 -0400

    Add banner pic and footer to home page

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,15 +1,4 @@
 $(document).on('ready page:load', function(){
-  // var $productCard = $('.products').masonry({
-  //     itemSelector: '.product-card',
-  //     columnWidth: 153.406,
-  //     isFitWidth: true
-  //   });
-
-  // $productCard.imagesLoaded().progress(function(){
-  //   $productCard.masonry('layout');
-  // });
-
-
   var productCard = $('#products').masonry({
     itemSelector: '.product-card',
     columnWidth: 147,
